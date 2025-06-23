17e4cbed611a9a4cd469e7f005bce37f4f763c30
Yolkkaii <drifter_2010@outlook.com>
2025-06-23 15:24:39 +0700

    Cell 01, Exercise 00

---
+++ cell01/ex00/name.py
@@ -0,0 +1,6 @@
+#create variables for my first name and last name
+first_name = "Ananda"
+last_name = "Stallard"
+
+#print out the variables for my first name and last name
+print (first_name, last_name)
