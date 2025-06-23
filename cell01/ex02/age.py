28e76f93c5925e82e96f5ef654b8e64c4372f847
Yolkkaii <drifter_2010@outlook.com>
2025-06-23 16:29:15 +0700

    Cell 01 Exercise 02 : Adding Numbers.

---
+++ cell01/ex02/age.py
@@ -0,0 +1,9 @@
+#create age and number variables to be added together
+age = 18
+number = 42
+
+#create variable to add age with 42
+my_age = age + number
+
+#display the third variable
+print (my_age)
