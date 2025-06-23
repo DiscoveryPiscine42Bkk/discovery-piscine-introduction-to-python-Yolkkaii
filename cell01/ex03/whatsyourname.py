0a03eebd338aeb598d0a2dd3b14bb026bd55fa10
Yolkkaii <drifter_2010@outlook.com>
2025-06-23 19:17:57 +0700

    Cell 01 Exercise 03 : What's your name!

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,4 @@
+first_name = input("Hello Evaluator, what's your first name?\nFirst name: ")
+last_name = input("And as for your last name?\nLast name: ")
+
+print(f"Well then, it's nice to meet you {first_name} {last_name}!")
