8bca748e7b243d8edb7ef35919016c77ea0c7dd4
Yolkkaii <drifter_2010@outlook.com>
2025-06-23 16:07:46 +0700

    Cell 01 Exercise 01: Concatenation!

---
+++ cell01/ex01/name.py
@@ -0,0 +1,9 @@
+#create variables for my first name and last name
+first_name = "Ananda"
+last_name = "Stallard"
+
+#concatenate the first and last name into one variable
+whole_name = first_name + ' ' + last_name
+
+#print out the whole name varible
+print (whole_name)
