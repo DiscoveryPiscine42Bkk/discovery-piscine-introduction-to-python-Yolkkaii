8392435fc6dcd01c8ab98c6d52bd07c78d906e52
Yolkkaii <drifter_2010@outlook.com>
2025-06-23 19:18:33 +0700

    Cell 01, Exercise 03: What's your name!

--- cell01/ex03/whatsyourname.py
+++ cell01/ex03/whatsyourname.py
@@ -1,4 +1,6 @@
+#create variables that asks for the user's first and last name
 first_name = input("Hello Evaluator, what's your first name?\nFirst name: ")
 last_name = input("And as for your last name?\nLast name: ")
 
+#says it's nice to meet them!
 print(f"Well then, it's nice to meet you {first_name} {last_name}!")
