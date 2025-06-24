56ffd03b25e6da67b42abaeeeb65d89675cd12d9
Yolkkaii <drifter_2010@outlook.com>
2025-06-24 16:37:18 +0700

    Cell 03, Exercise 02: I got that!

--- cell03/ex02/i_got_that.py
+++ cell03/ex02/i_got_that.py
@@ -3,7 +3,6 @@ text = str(input("What you gotta say?\n"))
 
 #while loop to keep asking until "STOP" is entered
 while text != "STOP":
-    input("I got that! Anything else?\n")
-    break
+    text = input("I got that! Anything else?\n")
 
 input("\nPress Enter to exit the program")
