aa9b1688644460816e3c3bc850ca6995d7e7914e
Yolkkaii <drifter_2010@outlook.com>
2025-06-24 16:25:58 +0700

    Cell 03, Exercise 02: I got that!

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,9 @@
+#variable to get user input
+text = str(input("What you gotta say?\n"))
+
+#while loop to keep asking until "STOP" is entered
+while text != "STOP":
+    input("I got that! Anything else?\n")
+    break
+
+input("\nPress Enter to exit the program")
